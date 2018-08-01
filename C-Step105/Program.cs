0d43293c45afd675122d71bfcd1c2a5aeec42112
ckaatz <39869540+ckaatz@users.noreply.github.com>
2018-07-31 22:13:37 -0500

    Add project files.

---
+++ C-Step105/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Step105
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> Division = new List<int> { 10, 20, 50 };
+                Console.WriteLine("Pick a number to divide the list by:");
+                int Num1 = Convert.ToInt32(Console.ReadLine());
+
+                foreach (int Div in Division)
+                {
+                    Console.WriteLine("No errors occurred!");
+                    Console.WriteLine(Div / Num1);
+                }
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by 0.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please type a whole number.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
